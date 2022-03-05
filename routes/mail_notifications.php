a96d109bb9033abde3736b5738285d5272e6238a
mahmoudmohamedramadan <mahmoudramadan496@gmail.com>
2022-03-05 13:05:22 +0200

    Customize the code and fix the typos

--- routes/mail_notifications.php
+++ routes/mail_notifications.php
@@ -1,14 +1,12 @@
 <?php
 
-use App\Models\User;
-use App\Mail\FirstMail;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MailController;
-use App\Http\Controllers\NotificationsController;
+use App\{Models\User, Mail\FirstMail};
+use App\Http\Controllers\{MailController, NotificationsController};
 
 Route::get('first_mail', [MailController::class, 'index']);
 
-/* when you're developing emails in your applications, it's helpful to be able to preview how they'll render. you can rely on a tool like Mailtrap for this, and that is a useful tool, BUT it can also be helpful to render the mails directly in your browser and see your changes made immediately */
+/* When you're developing emails in your applications, it's helpful to be able to preview how they'll render. you can rely on a tool like `Mailtrap` for this, and that is a useful tool, BUT it can also be helpful to render the mails directly in your browser and see your changes made immediately */
 
 Route::get('preview_email', function () {
     return new FirstMail(User::first());
