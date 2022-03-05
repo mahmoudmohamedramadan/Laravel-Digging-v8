ae5abea05a7146ef34959caad417b00f47d9b2b7
mahmoudmohamedramadan <mahmoudramadan496@gmail.com>
2022-03-05 07:30:55 +0200

    Fix the issue and some typos

--- app/Http/Controllers/NewFeaturesController.php
+++ app/Http/Controllers/NewFeaturesController.php
@@ -14,7 +14,7 @@ class NewFeaturesController extends Controller
      */
     public function index()
     {
-        /* The `retry` function attempts to execute the given callback until the given maximum attempt threshold is met. If the callback does not throw an exception, its return value will be returned. If the callback throws an exception, it will automatically be retried. If the maximum attempt count is exceeded, the exception will be thrown, NOTE that you can pass in the third paramter a closure  to manually calculate the number of milliseconds to sleep in between attempts and optionally pass a closure in the fourth parameter and accept the `$exception` */
+        /* The `retry` method attempts to execute the given callback until the given maximum attempt threshold is met. If the callback does not throw an exception, its return value will be returned. If the callback throws an exception, it will automatically be retried. If the maximum attempt count is exceeded, the exception will be thrown, NOTE that you can pass in the third paramter a closure  to manually calculate the number of milliseconds to sleep in between attempts and optionally pass a closure in the fourth parameter and accept the `$exception` */
         return retry(5, function () {
             // Attempt 5 times while resting 100ms in between attempts...
             return User::get();
@@ -39,8 +39,10 @@ class NewFeaturesController extends Controller
      */
     public function store(Request $request)
     {
-        /* If ou want check something in the request, instead of using multiple `if` you can use any scenairo of these */
+        /* If you want check something in the request, instead of using multiple `if` you can use any scenairo of these */
 
+        $model = '';
+        
         /* First you can use switvh case */
         switch($request->url()) {
             case '/users/create': $model = '\\App\\Models\\User';
