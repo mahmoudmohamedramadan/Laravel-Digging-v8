fcab45eef94dfbe4a5283d163b5beca5c29c77a3
mahmoudmohamedramadan <mahmoudramadan496@gmail.com>
2022-05-12 21:01:19 +0200

    Formatting

--- app/Http/Collections/DepartmentCollection.php
+++ app/Http/Collections/DepartmentCollection.php
@@ -11,7 +11,7 @@ class DepartmentCollection extends Collection
         /* If you want to return all collection type... return `$this`
          using `$carry` and `$item` allows you to show all items in the collection, `$carry` used to carry the result of the last iteration...for more info...https://riptutorial.com/laravel/example/12397/using-reduce--#:~:text=Laravel%20Using%20reduce()&text=The%20reduce%20method%20reduces%20the,result%20to%20the%20next%20iteration. */
         return $this->reduce(function ($carry, $item) {
-            return  $item['name'] .' >> '.$carry;
+            return  "{$item['name']} >> {$carry}";
         });
     }
 }
