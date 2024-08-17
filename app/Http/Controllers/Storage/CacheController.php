3fed4f56ad74368ae15a8dbbe795c8d2031c52f9
mahmoudmohamedramadan <mahmoudramadan496@gmail.com>
2024-08-17 10:36:33 +0300

    Update the storage content

--- app/Http/Controllers/Storage/CacheController.php
+++ app/Http/Controllers/Storage/CacheController.php
@@ -10,11 +10,11 @@ class CacheController extends Controller
     /* The biggest difference between `Cache` and `Session`, is the data in cache is cached per application, and the data in a session is cached per user. That means caches are more commonly. The cache configuration settings are available at `config/cache.php` */
     public function index()
     {
-        // You can use `Cache` facades or you can use the `cache` heper method to access the cache
+        // You can use the `Cache` facade or you can use the `cache` helper method to access the cache
         // Cache::get('users');
         // cache('$key', 'defaultCacheHelperValue');
 
-        /* You can set `keys` and `values` in the cache as first parameter and the second param is the `duration` of cache to leave */
+        /* You can set the `keys` and `values` in the cache as first parameter and the second param is the `duration` of cache to leave */
         // cache(['newCacheKey' => 'newCacheValue'], 10000);
 
         // This is another syntax to save data in cache
@@ -23,28 +23,32 @@ class CacheController extends Controller
         // This is another syntex to get cache value
         $aliasCacheHelperValue = cache()->get('newCacheKey', 'defaultCacheHelperValue');
 
-        /* `get` method makes it easy to retrieve the value for any given key, `pull` method is the same as the `get` method except it remove the cached value after retrieving it */
+        /* The `get` method makes it easy to retrieve the value for any given key, `pull` method is the same as the `get` method except it remove the cached value after retrieving it */
         // cache()->pull('$key', '$fallbackValue');
 
-        /* `add` method is similar to the `put` method, except if the value is already exists, it won't set it. Also, the method returns a Boolean indicating whetheror not the value was actually added */
+        /* The `add` method is similar to the `put` method, except if the value is already exists, it won't set it. Also, the method returns a Boolean indicating whetheror not the value was actually added */
         // cache()->add('$key', '$value');
 
-        /* `forever` method saves a value to the cache for a specfic key; it's the same the `put` method, except the value will never expire (until they're removed with `forget` method) */
+        /* The `forever` method saves a value to the cache for a specfic key; it's the same the `put` method, except the value will never expire (until they're removed with `forget` method) */
         // cache()->forever('$key', '$value');
 
-        // `has` method returns a Booleans indicating whetheror not there's a value set the provided key
+        // The `has` method returns a boolean indicating whether or not there's a value set in the provided key
         // cache()->has('$key');
 
-        /* `remember` method check a value exists in the cache for a certain key; and if does not, get that value somehow, save it to the cache, and return it, also it provide the number of seconds to leave and, a closure to define how to look it up, in case the key has no value set. The `rememberForever` method is the same, except it does not need you to set the number of seconds to leave */
+        /* The `remember` method check a value exists in the cache for a certain key; and if does not, get that value somehow, save it to the cache, and return it, also it provide the number of seconds to leave and, a closure to define how to look it up, in case the key has no value set */
         // cache()->remember('$key', '$second', '$closure');
+
+        // The `rememberForever` method is the same, except it does not need you to set the number of seconds to leave
         // cache()->rememberForever('$key', '$closure');
 
-        /* `increment` method allow you to increment integer value in cache. If there is no value at the given key, it'll be treated as if it were `0`, and if you pass a second param to increment or decrement, it'll increment or decrement by that amount instead of by `1` */
+        /* The `increment` method allows you to increment integer value in cache. If there is no value at the given key, it'll be treated as if it were `0`, and if you pass a second param to increment or decrement, it'll increment or decrement by that amount instead of by `1` */
         // cache()->increment('$key', '$amount');
         // cache()->decrement('$key', '$amount');
 
-        /* `forget` method works just like Session's `forget` method: pass it a key and it'll wipe that's key's value. The `flush` method wipes the entire cache */
+        // The `forget` method wipes The given key's value.
         // cache()->forget('$key');
+
+        // The `flush` method wipes the entire cache
         // cache()->flush();
 
         return view('cache.index', ['value' => $aliasCacheHelperValue]);
--- app/Http/Controllers/Storage/SessionController.php
+++ app/Http/Controllers/Storage/SessionController.php
@@ -10,7 +10,7 @@ class SessionController extends Controller
 
     public function index()
     {
-        // `put` method allows you to store key with its vlaue in session
+        // The `put` method allows you to store key with its vlaue in session
         session()->put('welcomeMsg', 'Hola User!');
 
         // You can use the below syntax also
@@ -21,46 +21,47 @@ class SessionController extends Controller
 
     public function sessionMethods()
     {
-        /* `get` method pulls the value of the provided key out of the session. If there is no value attached to that key, it will return the fallback value (and if you do not provide fallback will return `null`) */
+        /* The `get` method pulls the value of the provided key out of the session. If there is no value attached to that key, it will return the fallback value (and if you do not provide fallback will return `null`) */
         session()->get('$key', '$fallbackValue');
 
-        // `put` method stores the provided value in the session at the provided key
+        // The `put` method stores the provided value in the session at the provided key
         session()->put('$key', '$value');
 
-        /* `put` method allows you to add array value to the key while the `push` method will push a new value to exitance key of array */
+        /* NOTE: The `put` method allows you to add array value to the key while the `push` method will push a new value to exitance key of array */
         // session()->put('welcomeMsg', ['Welcome User!', 'Hola User!']);
         // session()->push('welcomeMsg', 'مرحبا');
 
-        // `has` method checks if session has value of specified key
+        // The `has` method checks if session has value of specified key
         session()->has('$key');
 
-        // `exists` method checks if the session has specified key
+        // The `exists` method checks if the session has specified key
         session()->exists('$key');
 
-        // `all` method returns an array of everything that's in the session
+        // The `all` method returns an array of everything that's in the session
         session()->all();
 
-        // `forget` method removes a previously value of passed key
+        // The `forget` method removes a previously value of passed key
         session()->forget('$key');
 
-        // `flush` method removes every session value, even those set by laravel like `_token`
+        // The `flush` method removes every session value, even those set by laravel like `_token`
         session()->flush();
 
-        // `pull` method is the same as `get` method, except that it deletes the value from the session after pulling it
+        /* The `pull` method is the same as `get` method, except that it deletes the value from the session after pulling it */
         session()->pull('$key', '$value');
 
         // It's not common, but if you need to regenerate your session ID, this method is there for you
         session()->regenerate();
 
-        /* one very common pattern for session storage is to set a value that you only want available for the next page load, exe: you might want to store a message like `user updated`. you could manually get that message and then wipe it on the next page load, but if you use this pattern a lot it can be wastful */
+        /* One very common pattern for session storage is to set a value that you only want available for the next page load, exe: you might want to store a message like `user updated`. you could manually get that message and then wipe it on the next page load, but if you use this pattern a lot it can be wastful */
 
-        // `flash` method sets the session key to the provided value for just the next page request
+        // The `flash` method sets the session key to the provided value for just the next page request
         session()->flash('$key', '$value');
 
         /* If you need the previous page's flash session data to stick around for one more request use `reflash` method to restore all of it for the next request */
         session()->reflash('$key', '$value');
 
-        // restore a single flash value for the next request, `keep` method can also accept an array
+        // If you want to restore a single flash value for the next request, you must use the `keep` method
+        // NOTE: The `keep` method can also accept an array
         session()->keep('$key');
     }
 }
